76fd2b1ea3074cb726497b43f08d4a7e3c910ca1
temkarus0070 <pretkovartyom@gmail.com>
2021-09-11 19:40:16 +0300

    small refactor

--- src/main/java/org/temkarus0070/MvcApp/controllers/AuthorizationController.java
+++ src/main/java/org/temkarus0070/MvcApp/controllers/AuthorizationController.java
@@ -2,11 +2,11 @@ package org.temkarus0070.MvcApp.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.temkarus0070.MvcApp.models.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.temkarus0070.MvcApp.dao.RegisterDAO;
 import org.temkarus0070.MvcApp.dao.Repositories.UserRepository;
+import org.temkarus0070.MvcApp.models.GrantedAuthority;
 import org.temkarus0070.MvcApp.models.MyUserDetails;
 import org.temkarus0070.MvcApp.models.User;
 
@@ -26,7 +26,7 @@ public class AuthorizationController {
 
     @Autowired
     public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder=passwordEncoder;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Autowired
@@ -40,28 +40,26 @@ public class AuthorizationController {
     }
 
 
-
     @PostMapping(path = "/register")
-    public void register(@RequestBody MyUserDetails myUserDetails){
-        List<GrantedAuthority> grantedAuthorityCollection=new LinkedList<>();
-        grantedAuthorityCollection.add( new GrantedAuthority("admin"));
-        String encodedPassword =passwordEncoder.encode(myUserDetails.getPassword());
+    public void register(@RequestBody MyUserDetails myUserDetails) {
+        List<GrantedAuthority> grantedAuthorityCollection = new LinkedList<>();
+        grantedAuthorityCollection.add(new GrantedAuthority("admin"));
+        String encodedPassword = passwordEncoder.encode(myUserDetails.getPassword());
         myUserDetails.setPassword(encodedPassword);
-        User user=new User(myUserDetails);
+        User user = new User(myUserDetails);
         user.setAuthorities(grantedAuthorityCollection);
         userRepository.save(user);
 
     }
 
     @GetMapping(path = "/role")
-    public String getRole(Principal principal){
-        User u=userRepository.getById(principal.getName());
-        if(u!=null){
+    public String getRole(Principal principal) {
+        User u = userRepository.getById(principal.getName());
+        if (u != null) {
             return u.getAuthorities().stream().findFirst().get().getAuthority();
         }
         return "";
     }
-
 
 
 }
