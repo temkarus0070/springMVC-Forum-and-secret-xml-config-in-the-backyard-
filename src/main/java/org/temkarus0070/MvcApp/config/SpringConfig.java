1c0c65520fc50faa726a2b22b0d27b96152c1a45
temkarus0070 <pretkovartyom@gmail.com>
2021-11-02 14:53:25 +0300

    repair app

--- src/main/java/org/temkarus0070/MvcApp/config/SpringConfig.java
+++ src/main/java/org/temkarus0070/MvcApp/config/SpringConfig.java
@@ -61,7 +61,7 @@ public class SpringConfig extends WebMvcConfigurationSupport{
     public DataSource dataSource(){
         DriverManagerDataSource dataSource=new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://192.168.1.126:5432/postgres");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
         dataSource.setUsername("postgres");
         dataSource.setPassword("postgres");
         return dataSource;
